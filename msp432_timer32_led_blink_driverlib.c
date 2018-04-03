d9accbb0dc5f720150fe60d9decc86e7ccaa9f96
rsp <siripokarpirom@gmail.com>
2018-04-03 15:29:23 +0700

    Add files via upload

--- msp432_timer32_led_blink_driverlib.c
+++ msp432_timer32_led_blink_driverlib.c
@@ -16,28 +16,28 @@ void GPIO_init() {
 }
 
 void Timer32_init() {
-    // Assume 48MHz System Clock Frequency
+    // Assume 12MHz System Clock Frequency
     MAP_Timer32_initModule(
        TIMER32_0_BASE,          // use the Timer32 module 0
-       TIMER32_PRESCALER_16,    // set the prescaler (1,16,256): 16
-       TIMER32_32BIT,           // use 16-bit or 32-bit mode: 32-bit
+       TIMER32_PRESCALER_256,   // set the prescaler (1,16,256): 16
+       TIMER32_16BIT,           // use 16-bit or 32-bit mode: 16-bit
        TIMER32_FREE_RUN_MODE    // use free-running or periodic mode: free-running
     );
-    MAP_Timer32_startTimer( TIMER32_0_BASE, true );
+    MAP_Timer32_startTimer( TIMER32_0_BASE, false ); // continuous mode
 }
 
 int main(void) {
-    uint32_t ts;
+    uint32_t ts, new_ts;
 
     MAP_WDT_A_holdTimer();        // stop WDT (watchdog timer)
     GPIO_init();                  // initialize the GPIO (LED pin)
     Timer32_init();               // initialize Timer32 module 0
 
-    ts = MAP_Timer32_getValue( TIMER32_0_BASE );
-
+    ts = MAP_Timer32_getValue(TIMER32_0_BASE);
     while(1)  {
-        if ( MAP_Timer32_getValue( TIMER32_0_BASE ) - ts >= 3000000UL ) {
-            ts += 300000UL;
+        new_ts = MAP_Timer32_getValue(TIMER32_0_BASE);
+        if ( ((uint16_t)(ts - new_ts)) >= 23437 ) {
+            ts = new_ts;
             MAP_GPIO_toggleOutputOnPin( GPIO_PORT_P1, GPIO_PIN0 ); // toggle the LED
         }
     }
