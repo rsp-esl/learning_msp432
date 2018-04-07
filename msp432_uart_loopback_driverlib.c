bcb82dffc3b6e75bed59844c5cb73eac5c659951
rsp <siripokarpirom@gmail.com>
2018-04-07 14:10:44 +0700

    Add files via upload

--- msp432_uart_loopback_driverlib.c
+++ msp432_uart_loopback_driverlib.c
@@ -31,11 +31,12 @@ eUSCI_UART_Config uartConfig = {
 void UART_init() {
     MAP_UART_initModule( EUSCI_A_MODULE, &uartConfig );
     MAP_UART_enableModule( EUSCI_A_MODULE );
-}
-void GPIO_init() {
     // Selecting P1.2 and P1.3 in UART mode
     MAP_GPIO_setAsPeripheralModuleFunctionInputPin( GPIO_PORT_P1,
              GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION );
+}
+
+void GPIO_init() {
     // P1.1 (LED pin) as output
     MAP_GPIO_setAsOutputPin( GPIO_PORT_P1, GPIO_PIN0 );
     MAP_GPIO_setOutputLowOnPin( GPIO_PORT_P1, GPIO_PIN0 );
